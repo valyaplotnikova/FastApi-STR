706022248c3f5b17643c8d0ac246c2ca958e1867
valya_plotnikova <valya_ezhova@mail.ru>
2025-01-22 15:10:03 +0300

    add lifespan and orjson to main

--- spimex_trading_app/main.py
+++ spimex_trading_app/main.py
@@ -1,13 +1,28 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from core.config import settings
 
 from api import router as api_router
+from core.models import db_helper
 
 
-app = FastAPI()
-app.include_router(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+main_app = FastAPI(
+    lifespan=lifespan,
+    default_response_class=ORJSONResponse,
+)
+
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
@@ -15,7 +30,7 @@ app.include_router(
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True
