b1ca2710e9a8e4fa6b33331c0f2fe05a9959fd58
valya_plotnikova <valya_ezhova@mail.ru>
2025-01-23 15:48:23 +0300

    add cache to main

--- spimex_trading_app/main.py
+++ spimex_trading_app/main.py
@@ -1,37 +1,33 @@
 from contextlib import asynccontextmanager
 
-import uvicorn
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
+
+# from fastapi.responses import ORJSONResponse
 
 from core.config import settings
 
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 from api import router as api_router
 from core.models import db_helper
+from redis import asyncio as aioredis
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
+    redis = aioredis.from_url(f"{settings.celery.broker}://{settings.celery.port}")
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
     yield
     # shutdown
     await db_helper.dispose()
 
 main_app = FastAPI(
     lifespan=lifespan,
-    default_response_class=ORJSONResponse,
+    # default_response_class=ORJSONResponse,
 )
 
 main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
-
-
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:main_app",
-        host=settings.run.host,
-        port=settings.run.port,
-        reload=True
-    )
